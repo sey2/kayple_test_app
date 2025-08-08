ed8915723047d6b494fb329d6b2d5ad5d06319a2
박세영 <casio2978@naver.com>
2025-08-08 15:37:51 +0900

    [design] postList 디자인 작업

--- lib/presenter/post/screens/post_list_screen.dart
+++ lib/presenter/post/screens/post_list_screen.dart
@@ -1,10 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:kayple_test/data/post/repository/post_repository_impl.dart';
+import 'package:kayple_test/domain/post/entity/post_entity.dart';
+import 'package:kayple_test/presenter/base/base_state.dart';
+import 'package:kayple_test/presenter/post/view_models/post_list_view_model.dart';
+import 'package:provider/provider.dart';
 
 final class PostListScreen extends StatelessWidget {
   const PostListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: _PostListArea());
+  }
+}
+
+final class _PostListArea extends StatelessWidget {
+  const _PostListArea();
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create:
+          (context) => PostListViewModel(
+            postRepository: context.read<PostRepositoryImpl>(),
+          ),
+      builder: (context, child) {
+        final postListState = context.select<PostListViewModel, StateEnum>((
+          viewModel,
+        ) {
+          return viewModel.postListState.state;
+        });
+
+        switch (postListState) {
+          case StateEnum.initial:
+          case StateEnum.loading:
+            return const Center(child: CircularProgressIndicator());
+          case StateEnum.error:
+            final errorMessage =
+                context.read<PostListViewModel>().postListState.errorMessage;
+
+            return Center(child: Text('Error: $errorMessage'));
+          case StateEnum.success:
+            {
+              final postList = context
+                  .select<PostListViewModel, List<PostEntity>>((
+                    PostListViewModel viewModel,
+                  ) {
+                    return viewModel.postList;
+                  });
+
+              return ListView.separated(
+                padding: EdgeInsets.symmetric(horizontal: 14),
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Post ${postList[index].title}'),
+                    trailing: GestureDetector(
+                      onTap: () {
+                        context.read<PostListViewModel>().toggleFavorite(
+                          postList[index],
+                        );
+                      },
+                      child: Icon(
+                        Icons.favorite,
+                        size: 12,
+                        color:
+                            postList[index].isFavorite
+                                ? Colors.red
+                                : Colors.black,
+                      ),
+                    ),
+                    subtitle: Text(postList[index].body),
+                    onTap: () {
+                      // app TODO: 네비게이션 게시글 상세
+                    },
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Divider(color: Colors.grey.shade300, height: 1);
+                },
+                itemCount: postList.length,
+              );
+            }
+        }
+      },
+    );
   }
 }
