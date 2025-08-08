f1ad2e8765b7d538fe664f049e8190f7ed621ee4
박세영 <casio2978@naver.com>
2025-08-08 15:06:57 +0900

    [Setting] 메인 기본 설정

---
+++ lib/presenter/post/screens/post_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final class PostListScreen extends StatelessWidget {
+  const PostListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
